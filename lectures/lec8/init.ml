de410d72fe2a2b503713fac54090e2a7625c34d7
KC Sivaramakrishnan <kc@kcsrk.info>
2019-08-06 14:19:42 +0530

    reorg lec8

--- lectures/lec8/init.ml
+++ lectures/lec8/init.ml
@@ -1,8 +1,8 @@
 #require "str";;
-#directory "lambda/_build/src";;
-#load_rec "lambda/_build/src/syntax.cmo";;
-#load_rec "lambda/_build/src/lambda_parse.cmo";;
-#load_rec "lambda/_build/src/eval.cmo";;
+#directory "objects";;
+#load_rec "objects/syntax.cmo";;
+#load_rec "objects/lambda_parse.cmo";;
+#load_rec "objects/eval.cmo";;
 
 let eval s = s |> Lambda_parse.parse_string |> Eval.eval |> Syntax.string_of_expr 
 let eval_traced s = s |> Lambda_parse.parse_string |> Eval.eval ~log:true |> Syntax.string_of_expr 
--- lectures/lec8/lambda/myocamlbuild.ml
+++
@@ -1,6 +0,0 @@
-open Ocamlbuild_plugin;;
-
-Options.ocaml_pkgs := "str"::!Options.ocaml_pkgs;;
-Options.ocaml_cflags := "-bin-annot"::!Options.ocaml_cflags;;
-Options.use_ocamlfind := true;;
-
