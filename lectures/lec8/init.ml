c8cd25e993f65b82ec1ec6b24ad3334fd4cecdef
KC Sivaramakrishnan <kc@kcsrk.info>
2019-08-08 12:22:54 +0530

    Fix the code

--- lectures/lec8/init.ml
+++ lectures/lec8/init.ml
@@ -1,8 +1,22 @@
 #require "str";;
-#directory "objects";;
-#load_rec "objects/syntax.cmo";;
-#load_rec "objects/lambda_parse.cmo";;
-#load_rec "objects/eval.cmo";;
+#directory "/lectures/lambda";;
+#load_rec "/lectures/lambda/syntax.cmo";;
+#load_rec "/lectures/lambda/lambda_parse.cmo";;
+#load_rec "/lectures/lambda/eval.cmo";;
 
-let eval s = s |> Lambda_parse.parse_string |> Eval.eval |> Syntax.string_of_expr 
-let eval_traced s = s |> Lambda_parse.parse_string |> Eval.eval ~log:true |> Syntax.string_of_expr 
+(*
+let eval s = s |> Lambda_parse.parse_string |> Eval.eval |> Syntax.string_of_expr
+let eval_traced s = s |> Lambda_parse.parse_string |> Eval.eval ~log:true |> Syntax.string_of_expr
+*)
+
+open Syntax
+
+let parse = Lambda_parse.parse_string
+
+let free_variables s =
+  s |> Lambda_parse.parse_string |> Eval.free_variables |> Eval.SS.elements
+
+let substitute expr a b =
+  let expr = Lambda_parse.parse_string expr in
+  let b = Lambda_parse.parse_string b in
+  Eval.substitute expr a b |> Syntax.string_of_expr
